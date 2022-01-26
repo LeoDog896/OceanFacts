f92ed0aba0520914ea82496bb9a8fbcfa8fd147d
William Faircloth <58738637+willuhm-js@users.noreply.github.com>
2022-01-26 13:55:32 +0000

    🌊 Remove redundant script

--- src/public/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".infoToggle").click(function() {
-    let toggledText = $(this).text() === "Read more" ? "Read less" : "Read more";
-    $(this).text(toggledText);
-    $(".infoText").toggleClass("d-none");
-  });
-})
