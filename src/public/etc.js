94c77b6a0fbe504da241625d696904e3491a4093
William Faircloth <58738637+willuhm-js@users.noreply.github.com>
2022-02-01 02:20:11 +0000

    Add QR code to link share

--- src/public/etc.js
+++ src/public/etc.js
@@ -1,13 +1,15 @@
+const linkToCopy = $(location).attr('origin') + $(".copyButton").attr("dest");
+new QRCode(document.getElementById("qrcode"), linkToCopy);
+
 $(document).ready(function() {
   $(".copyButton").click(function() {
-    const linkToCopy = $(location).attr('origin') + $(this).attr("dest");
     var temp = $("<input style='display: hidden;'>");
     $("body").append(temp);
     temp.val(linkToCopy).select();
     document.execCommand("copy");
     temp.remove();
 
-    const toast = new bootstrap.Toast($(".toast"))
-    toast.show()
+    const toast = new bootstrap.Toast($(".toast"));
+    toast.show();
   });
 })
